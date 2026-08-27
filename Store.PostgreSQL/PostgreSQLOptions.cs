e130390c381ec1d25193ba29a16a797c7eb12b6c
olesya <bulchuk.olesya@gmail.com>
2026-08-28 00:03:37 +0700

    chore: add oprions and IServiceCollection extesions

---
+++ Store.PostgreSQL/PostgreSQLOptions.cs
@@ -0,0 +1,6 @@
+﻿namespace ResultMetrics.Store.PostgreSQL;
+
+public class PostgreSqlOptions
+{
+    public string ConnectionString { get; set; } = null!;
+}
---
+++ Store.PostgreSQL/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ResultMetrics.Store.PostgreSQL;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddPostgreSql(this IServiceCollection services, PostgreSqlOptions options)
+    {
+        services.AddDbContext<ResultMetricsDbContext>(dbOptions => dbOptions.UseNpgsql(options.ConnectionString));
+        return services;
+    }
+}
