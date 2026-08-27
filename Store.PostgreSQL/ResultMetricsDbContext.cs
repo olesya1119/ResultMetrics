ca4694d176a26f373f62985a0e6f64cc5c1bb25b
olesya <bulchuk.olesya@gmail.com>
2026-08-27 23:55:03 +0700

    chore: add DbContext

---
+++ Store.PostgreSQL/ResultMetricsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ResultMetrics.Store.PostgreSQL;
+
+public class ResultMetricsDbContext: DbContext
+{
+    public ResultMetricsDbContext(DbContextOptions<ResultMetricsDbContext> options) : base(options)
+    {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ResultMetricsDbContext).Assembly);
+    }
+}
