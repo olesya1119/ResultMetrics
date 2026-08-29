fae32aab34c1fc699a2a95562da439a6ea38a830
olesya <bulchuk.olesya@gmail.com>
2026-08-29 21:24:39 +0700

    feat: add ControllerExtensions

---
+++ Common/Result/ControllerExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ResultMetrics.Common.Result;
+
+public static class ControllerExtensions
+{
+    public static IActionResult ToActionResult(
+        this ControllerBase controller,
+        Result result)
+    {
+        if (result.Successful)
+        {
+            return controller.StatusCode((int)result.StatusCode);
+        }
+
+        return controller.StatusCode(
+            (int)result.StatusCode,
+            result.Fault);
+    }
+
+    public static IActionResult ToActionResult<TValue>(
+        this ControllerBase controller,
+        Result<TValue> result)
+    {
+        if (result.Successful)
+        {
+            return controller.StatusCode(
+                (int)result.StatusCode,
+                result.Value);
+        }
+
+        return controller.StatusCode(
+            (int)result.StatusCode,
+            result.Fault);
+    }
+}
