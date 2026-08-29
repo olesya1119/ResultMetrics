6dd2d08f32ff9b35576244b67f3d85e0212d2a95
olesya <bulchuk.olesya@gmail.com>
2026-08-29 19:55:52 +0700

    fix: поправила тип в модельках

--- Store.PostgreSQL/Models/Results.cs
+++ Store.PostgreSQL/Models/Results.cs
@@ -7,10 +7,10 @@ public class Results
 {
     public long Id { get; set; }
     public string FileName { get; set; } = null!;
-    public int DeltaTimeInSeconds { get; set; }
+    public double DeltaDateInSeconds { get; set; }
     public DateTime MinDate { get; set; }
-    public double AverageExecutionTime { get; set; }
-    public double AverageValue { get; set; }
+    public double AvgExecutionTime { get; set; }
+    public double AvgValue { get; set; }
     public double MedianValue { get; set; }
     public double MinValue { get; set; }
     public double MaxValue { get; set; }
@@ -21,10 +21,10 @@ public class Results
         {
             builder.HasKey(x => x.Id);
             builder.Property(x => x.FileName).IsRequired().HasMaxLength(100);
-            builder.Property(x => x.DeltaTimeInSeconds).IsRequired();
-            builder.Property(x => x.MinDate).IsRequired();
-            builder.Property(x => x.AverageExecutionTime).IsRequired();
-            builder.Property(x => x.AverageValue).IsRequired();
+            builder.Property(x => x.DeltaDateInSeconds).IsRequired();
+            builder.Property(x => x.MinDate).IsRequired().HasColumnType("timestamp without time zone");
+            builder.Property(x => x.AvgExecutionTime).IsRequired();
+            builder.Property(x => x.AvgValue).IsRequired();
             builder.Property(x => x.MedianValue).IsRequired();
             builder.Property(x => x.MinValue).IsRequired();
             builder.Property(x => x.MaxValue).IsRequired();
