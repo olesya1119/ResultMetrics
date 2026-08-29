d0a8ddefdca9b64247c9fc4d61e164101b6cb693
olesya <bulchuk.olesya@gmail.com>
2026-08-29 23:00:30 +0700

    feat: add GetLatestValuesQueryHandler

---
+++ Api.WebApi/Features/ResultMetrics/GetLatestValues/GetLatestValuesQuery.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using MediatR;
+using ResultMetrics.Api.WebApi.Models;
+using ResultMetrics.Common.Result;
+
+namespace ResultMetrics.Api.WebApi.Features.ResultMetrics.GetLatestValues;
+
+public class GetLatestValuesQuery : IRequest<Result<IReadOnlyCollection<ValueModel>>>
+{
+    /// <summary>
+    /// Имя файла.
+    /// </summary>
+    public string FileName { get; init; } = null!;
+
+    public class Validator : AbstractValidator<GetLatestValuesQuery>
+    {
+        public Validator()
+        {
+            RuleFor(x => x.FileName)
+                .NotEmpty()
+                .WithMessage("File name is required.");
+        }
+    }
+}
