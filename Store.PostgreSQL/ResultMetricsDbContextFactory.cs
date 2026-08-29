e97adfd21c23761253de9f87e62efc1f0c64ce33
olesya <bulchuk.olesya@gmail.com>
2026-08-29 19:59:03 +0700

    feat: factory and options for create migrations

---
+++ Store.PostgreSQL/ResultMetricsDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+using ResultMetrics.Common.Configuration;
+
+namespace ResultMetrics.Store.PostgreSQL;
+
+public class ResultMetricsDbContextFactory : IDesignTimeDbContextFactory<ResultMetricsDbContext>
+{
+    public ResultMetricsDbContext CreateDbContext(string[] args)
+    {
+        var services = new ServiceCollection();
+
+        services
+            .AddApplicationOptions<PostgreSqlOptions>()
+            .AddPostgreSql();
+        
+        var serviceProvider = services.BuildServiceProvider();
+
+        return serviceProvider .GetRequiredService<ResultMetricsDbContext>();
+    }
+}
+    
